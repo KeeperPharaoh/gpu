f9ea81db9908e0b2cfae7f2d9a4607bb011069ec
Farkhad <farhad.begimbetov1@gmail.com>
2025-07-10 09:54:26 +0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/stage-tlg-reg/',
+  base: '/gpu/',
   plugins: [react()],
   build: {
     outDir: 'dist',
